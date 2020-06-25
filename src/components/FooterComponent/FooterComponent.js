e1b391f177bd376ed7ae388ec93a22435b69b949
shawon39 <shshawon39@gmail.com>
2020-06-25 23:15:31 +0600

    Designing Footer

--- src/components/FooterComponent/FooterComponent.js
+++ src/components/FooterComponent/FooterComponent.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import './FooterComponent.css';
+import React from "react";
+import "./FooterComponent.css";
 
 function FooterComponent() {
-  return (
-    <div className="footer row">
-      Copyright@Shawon
-    </div>
-  )
+	return (
+		<div className="footer row">
+			<div className="col-md-4">
+				<h5>Contacts</h5>
+				<p>Shakhawat Hossain</p>
+				<p>shshawon39@gmail.com</p>
+				<p>01001010101010</p>
+			</div>
+			<div className="col-md-4">
+				<h5>Social Links</h5>
+				<p>facebook.com/sh.shawon1</p>
+				<p>github.com/shawon39</p>
+				<p>twitter.com/shshawon39</p>
+			</div>
+			<div className="col-md-4">
+				<h5>Company</h5>
+				<p>Advertise</p>
+				<p>Privacy Policy</p>
+				<p>Terms of use</p>
+			</div>
+			<p className="copyright">
+				The Hooks Planet || All rights reserved || The Planet
+			</p>
+		</div>
+	);
 }
 
-export default FooterComponent
+export default FooterComponent;
