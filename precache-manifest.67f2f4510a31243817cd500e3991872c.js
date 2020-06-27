45edf58765f9eea5b573446ddecb59e8829519eb
shawon39 <shshawon39@gmail.com>
2020-06-27 13:56:32 +0600

    Updates

---
+++ precache-manifest.67f2f4510a31243817cd500e3991872c.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b26b4585c07f776f2583e90cc9fa390",
+    "url": "/The-Hooks-Planet/index.html"
+  },
+  {
+    "revision": "b9a3ec82ede36b3c91c9",
+    "url": "/The-Hooks-Planet/static/css/main.92159390.chunk.css"
+  },
+  {
+    "revision": "3b5f8160680d33ceb67e",
+    "url": "/The-Hooks-Planet/static/js/2.772372fc.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/The-Hooks-Planet/static/js/2.772372fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a3ec82ede36b3c91c9",
+    "url": "/The-Hooks-Planet/static/js/main.396d6d02.chunk.js"
+  },
+  {
+    "revision": "6853afb9422c53348397",
+    "url": "/The-Hooks-Planet/static/js/runtime-main.c7438f12.js"
+  },
+  {
+    "revision": "4139fdd5dd47838cff2e25490561f960",
+    "url": "/The-Hooks-Planet/static/media/1.4139fdd5.png"
+  },
+  {
+    "revision": "415b275ba20e4cd5b6db62128fdae3bc",
+    "url": "/The-Hooks-Planet/static/media/1.415b275b.jpg"
+  },
+  {
+    "revision": "664c0cd61f30ea6f4767ba0fc46bc52c",
+    "url": "/The-Hooks-Planet/static/media/1.664c0cd6.jpg"
+  },
+  {
+    "revision": "ca1af137560114ca62ec3f4100692ab8",
+    "url": "/The-Hooks-Planet/static/media/1.ca1af137.jpg"
+  },
+  {
+    "revision": "6f32259ed385a446f7447d0d67717060",
+    "url": "/The-Hooks-Planet/static/media/10.6f32259e.jpg"
+  },
+  {
+    "revision": "8a4d4d1540a77bcec7d3b07787f29d36",
+    "url": "/The-Hooks-Planet/static/media/10.8a4d4d15.jpg"
+  },
+  {
+    "revision": "9a7b213bc4e913dde698206defa5f725",
+    "url": "/The-Hooks-Planet/static/media/10.9a7b213b.jpg"
+  },
+  {
+    "revision": "1796d764f6c15b721f6dfcab65139852",
+    "url": "/The-Hooks-Planet/static/media/2.1796d764.jpg"
+  },
+  {
+    "revision": "21bfea986576dd1740aa3b02f8631367",
+    "url": "/The-Hooks-Planet/static/media/2.21bfea98.jpg"
+  },
+  {
+    "revision": "2bf913b3cc95c3adc259d6f16091cc6a",
+    "url": "/The-Hooks-Planet/static/media/2.2bf913b3.jpg"
+  },
+  {
+    "revision": "a80e04933083aabfbedea1aba0ac59e2",
+    "url": "/The-Hooks-Planet/static/media/2.a80e0493.jpg"
+  },
+  {
+    "revision": "e0afe4b415e3ba7ac2990a7ed7361bd9",
+    "url": "/The-Hooks-Planet/static/media/2.e0afe4b4.jpg"
+  },
+  {
+    "revision": "0513bb8b5951bb50cad8608bb8866f32",
+    "url": "/The-Hooks-Planet/static/media/3.0513bb8b.jpg"
+  },
+  {
+    "revision": "18f040e1cffa42850c3b6c8e94f661f6",
+    "url": "/The-Hooks-Planet/static/media/3.18f040e1.jpg"
+  },
+  {
+    "revision": "699d511d27a5f40c57d1972582bcb01a",
+    "url": "/The-Hooks-Planet/static/media/3.699d511d.jpg"
+  },
+  {
+    "revision": "6a0d4111cbe36906ccc76796e4547201",
+    "url": "/The-Hooks-Planet/static/media/3.6a0d4111.jpg"
+  },
+  {
+    "revision": "93a6225f85a894e17f7f5efb2ed88c1d",
+    "url": "/The-Hooks-Planet/static/media/4.93a6225f.jpg"
+  },
+  {
+    "revision": "9440ccea5d732251cf2b55e968c32ea5",
+    "url": "/The-Hooks-Planet/static/media/4.9440ccea.jpg"
+  },
+  {
+    "revision": "be80469bfdf7b08b5cca6324639cdb12",
+    "url": "/The-Hooks-Planet/static/media/4.be80469b.jpg"
+  },
+  {
+    "revision": "5222f3aeb6b0c6ff92b5f29abdb8d4e3",
+    "url": "/The-Hooks-Planet/static/media/5.5222f3ae.jpg"
+  },
+  {
+    "revision": "abaabf21cee77f24e37ffd5af42d6445",
+    "url": "/The-Hooks-Planet/static/media/5.abaabf21.jpg"
+  },
+  {
+    "revision": "ff91bb75524c8e441be97d695818bb18",
+    "url": "/The-Hooks-Planet/static/media/5.ff91bb75.png"
+  },
+  {
+    "revision": "37cc601623ea657571ee1a0bb5b4668f",
+    "url": "/The-Hooks-Planet/static/media/6.37cc6016.jpg"
+  },
+  {
+    "revision": "51efa84719d1eedb159c02100d67a405",
+    "url": "/The-Hooks-Planet/static/media/6.51efa847.jpg"
+  },
+  {
+    "revision": "6bc4bdcf4d567692f62d15a630e95fb2",
+    "url": "/The-Hooks-Planet/static/media/6.6bc4bdcf.jpg"
+  },
+  {
+    "revision": "a0a7a37206b25d567dde8da3ea66c306",
+    "url": "/The-Hooks-Planet/static/media/6.a0a7a372.jpg"
+  },
+  {
+    "revision": "243d6846acbd792dec13b5daa8763abc",
+    "url": "/The-Hooks-Planet/static/media/7.243d6846.jpg"
+  },
+  {
+    "revision": "6a1f2eec7ea996eb565ae44ff59db623",
+    "url": "/The-Hooks-Planet/static/media/7.6a1f2eec.jpg"
+  },
+  {
+    "revision": "fccf42155617d1494dfba62f2474af54",
+    "url": "/The-Hooks-Planet/static/media/7.fccf4215.jpg"
+  },
+  {
+    "revision": "0e181955d4afd1f87b33c4c8ebfad737",
+    "url": "/The-Hooks-Planet/static/media/8.0e181955.jpg"
+  },
+  {
+    "revision": "f495177322fde94609b4ea0e3dba4c6f",
+    "url": "/The-Hooks-Planet/static/media/8.f4951773.jpg"
+  },
+  {
+    "revision": "2a32c368dfe9255a8f360dbcfc3e60fd",
+    "url": "/The-Hooks-Planet/static/media/9.2a32c368.png"
+  },
+  {
+    "revision": "e8347f4ed4950bfe8e31c658d4537041",
+    "url": "/The-Hooks-Planet/static/media/9.e8347f4e.png"
+  },
+  {
+    "revision": "d704bd89bfa1c4c59cbe39d47ecd6fdb",
+    "url": "/The-Hooks-Planet/static/media/no-image-available.d704bd89.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a21172492fa1caf49fcebc5279282c72.js"
+  "/The-Hooks-Planet/precache-manifest.67f2f4510a31243817cd500e3991872c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/The-Hooks-Planet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
